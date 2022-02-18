254be4aec95c5d4f00cc85631901f18cf5dd9431
Dell <Dell@DESKTOP-RDFCBQU>
2022-02-18 18:42:59 +0100

    Subir archivos a GitHub

---
+++ Proyecto1/src/proyecto1/Cliente.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+/**
+ *
+ * @author Dell
+ */
+public class Cliente extends Persona {
+   
+
+    public Cliente(String nombre, int edad) {
+        super(nombre,edad);
+    }
+
+    public Cliente() {
+    }
+    
+   
+    
+}
